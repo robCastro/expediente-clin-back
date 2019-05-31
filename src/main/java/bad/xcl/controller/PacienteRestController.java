4c646c6be58490a6544b0d4b5dc7870188d5d98b
brenNavas <32327822+brenNavas@users.noreply.github.com>
2019-05-30 18:08:34 -0600

    crear paciente

--- src/main/java/bad/xcl/controller/PacienteRestController.java
+++ src/main/java/bad/xcl/controller/PacienteRestController.java
@@ -1,12 +1,19 @@
 package bad.xcl.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +49,26 @@ public class PacienteRestController {
 		return pacientes;
 	}
 
+	//crear nuevo paciente
+	@PostMapping("/crear")
+	public ResponseEntity<?> create(@RequestBody Paciente paciente)
+	{
+		Paciente pacienteNuevo = null;
+		Map<String, Object> response  = new HashMap<>();
+		
+		try {
+			paciente.setId(pacienteService.generarId());
+			pacienteNuevo = pacienteService.save(paciente);
+		} catch (DataAccessException e) {
+			response.put("mensaje","Error al realizar al insertar en la base de datos.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El paciente ha sido creado con éxito");
+		response.put("estado", pacienteNuevo);
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
 }
